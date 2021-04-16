419aafc8f384c7786c9b73d93d41736edb3226ac
Owais <precisework01@gmail.com>
2021-04-16 08:26:35 +0500

    update code

--- middleware/database.js
+++ middleware/database.js
@@ -3,11 +3,11 @@ import nextConnect from 'next-connect';
 
 const { MONGODB_URI, MONGODB_DB } = process.env
 
-const uri = "mongodb+srv://123321:123321@gql-owais.ntqz7.mongodb.net/MCT?retryWrites=true&w=majority";
+const uri =MONGODB_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect(err => {
-  const collection = client.db("MCT").collection("daily");
+  const collection = client.db(MONGODB_DB).collection("daily");
   // perform actions on the collection object
 
 });
@@ -15,7 +15,7 @@ client.connect(err => {
 async function database(req, res, next) {
   if (!client.isConnected()) await client.connect();
   req.dbClient = client;
-  req.db = client.db('MCT');
+  req.db = client.db(MONGODB_DB );
   
   return next();
 }
